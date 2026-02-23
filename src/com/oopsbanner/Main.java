aaa417bc641f56950f56b4115e14ccea2d598652
Your Name <your.email@example.com>
2026-02-23 10:07:13 +0530

    UC1 - Print OOPS

--- src/com/oopsbanner/Main.java
+++ src/com/oopsbanner/Main.java
@@ -2,7 +2,7 @@ package com.oopsbanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to OOPS Banner App");
+        System.out.println("OOPS");
     }
 }
 
